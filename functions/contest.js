67e8f416a0fe463f9db78f4ead67e4ab3f4e35ab
mezin <z2941@ya.ru>
2024-04-05 21:01:17 +0300

    Скорректировал алгоритм и реализацию подведения итогов

--- functions/contest.js
+++ functions/contest.js
@@ -42,6 +42,8 @@ exports.handler = async function (event, ctx) {
 
 		let user = await getUserById(user_id);
 		let chatMember = await getChatMemberByUserId(user_id, chat_id);
+		let chats = await client.from('chats').upsert(chat_member.chat);
+
 		// console.log('user ', user);
 
 		if (user) {
@@ -139,20 +141,13 @@ exports.handler = async function (event, ctx) {
 		}
 
 		if (text == '/runtest') {
-			// return { statusCode: 200 };
-
 			//1. Запускаем скрипт каждые 3 минуты (ставим в крон задание)
 			//2. Скрипт запрашивает конкурсы, в которых статус pending и start_date > now() (func get_start_all_date)
 			//3. Если массив конкурсов не пустой - запрашиваем в цикле chat_member со статусом member и выбираем рандомно winner_count
 
-			// const { data, error } = await client.from('contest_config').select('*, chat_member (id, new_status)');
-			// console.log('data ', data);
-
-			// return { statusCode: 200 };
-
-			//return start_day[]
+			//return finish_date[]
 			const { data: finish_date_array, error: finishDateArrayError } = await client
-				.rpc('get_start_all_date')
+				.rpc('get_all_finish_date')
 				.select('title, status, winner_count, chat_id')
 				.eq('status', 'pending');
 
@@ -160,62 +155,30 @@ exports.handler = async function (event, ctx) {
 
 			//3. Если массив конкурсов не пустой - проходим в цикле по конкурсам и последовательно запрашиваем chat_member со статусом member и выбираем рандомно winner_count
 			if (finish_date_array.length > 0) {
-				// console.log('finish_date_array ', finish_date_array);
-
-				//проходим в цикле по конкурсам и последовательно запрашиваем chat_member со статусом member
 				for await (const contest of finish_date_array) {
-					console.log('contest ', contest);
-					//return user_id[]
-					const { data: winner, error: winnerError } = await client
-						.from('chat_member')
-						.select('new_status')
-						.eq('new_status', 'member')
-						.eq('chat_id', contest.chat_id)
-						.select('user_id');
-					console.log('winners ', winner);
+					const { data: winners, error: winnerError } = await client.rpc('get_random_users_winner', { chatid: contest?.chat_id });
 
-					if (winner?.length > 0) {
-						//return users[]
-						const { data: users, error: usersError } = await client
-							.from('users')
-							.select('*')
-							.in(
-								'user_id',
-								winner.map((i) => i.user_id)
-							);
-						console.log('users ', users);
-
-						winner_message.push(
+					if (winners?.length) {
+						await sendMessage(chat.id, `Поздравляем победителей:\n${winner_message.join('\n')}`);
+						await sendMessage(
+							chat.id,
 							`Конкурс "${contest.title}":\n${
-								users.length == 0 ? 'Нет победителей\n' : users.map((i, _) => `${++_}. ${i.first_name} ${i.last_name}`).join('\n')
+								winners.length == 0 ? 'Нет победителей\n' : winners.map((i, _) => `${++_}. ${i.first_name} ${i.last_name}`).join('\n')
 							}`
 						);
+
+						// winner_message.push(
+						// 	`Конкурс "${contest.title}":\n${
+						// 		winners.length == 0 ? 'Нет победителей\n' : winners.map((i, _) => `${++_}. ${i.first_name} ${i.last_name}`).join('\n')
+						// 	}`
+						// );
 					}
 				}
 			} else {
 				return { statusCode: 200 };
 			}
-			// console.log('winner_message ', winner_message);
 
-			//return user_id[]
-			// const { data: winner, error: winnerError } = await client
-			// 	.from('chat_member')
-			// 	.select('new_status')
-			// 	.eq('new_status', 'left')
-			// 	.select('user_id');
-
-			// console.log('winner ', winner);
-
-			// //return users[]
-			// const { data: users, error: usersError } = await client
-			// 	.from('users')
-			// 	.select('*')
-			// 	.in(
-			// 		'user_id',
-			// 		winner.map((i) => i.user_id)
-			// 	);
-
-			await sendMessage(chat.id, `Поздравляем победителей:\n${winner_message.join('\n')}`);
+			// await sendMessage(chat.id, `Поздравляем победителей:\n${winner_message.join('\n')}`);
 		}
 
 		if (text === '/allcontest' && state === null) {
@@ -284,8 +247,8 @@ exports.handler = async function (event, ctx) {
 			await sendMessage(
 				chat.id,
 				`Введите id канала или группы
-			\nЕсли канал - то можно просто переслать сюда любое сообщение из целевого канала
-			\nЕсли группа - то введите команду /id непосредственно в целевой группе`,
+			\nЕсли канал - то можно просто переслать боту любое сообщение из целевого канала
+			\nЕсли группа - то введите команду <code>/id</code> непосредственно в целевой группе`,
 				{
 					inline_keyboard: [[{ text: 'Отмена', callback_data: '/cancel' }]],
 				}
@@ -364,7 +327,7 @@ exports.handler = async function (event, ctx) {
 			await sendMessage(
 				chat.id,
 				`Параметры конкурса:\nНазвание: ${contest.title}\nТекст объявления: ${contest.message_start}
-        \nСроки проведения: ${new Date(contest.start_date).toLocaleString()} - ${new Date(contest.finish_date).toLocaleString()}
+        \LПодведение итогов: ${new Date(contest.finish_date).toLocaleString()}
         \nКоличество победителей: ${contest.winner_count}
         \nСсылка для метки: <code>${contest.id}</code> <i>(можно нажать для копирования)</i>
         \nhttps://t.me/freecontest_bot?start=${contest.id}
