2898589bac3f9659a3352398ab86641306d950a0
mezin <z2941@ya.ru>
2024-04-14 14:56:23 +0300

    feat: fetch coef

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -3,33 +3,26 @@ const client = require('../lib/supabase/wb_acceptance/client');
 
 export default async (req) => {
 	console.log('req ', req.json());
-	// const { next_run } = await req.json();
 
-	const { data, error } = await client.from('requests').select('*');
+	const { data, error } = await client.from('requests').select('*').eq('is_active', true);
 
-	console.log('data ', data[0]);
-	console.log('error ', error);
-
-	// const response = await fetch('https://coef.wbcon.su/get_coef', {
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Content-Type': 'application/json',
-	// 	},
-	// 	body: JSON.stringify({
-	// 		wh_id: request[0].wh_id,
-	// 		delivery_date: request[0].delivery_date,
-	// 		delivery_type: request[0].delivery_type,
-	// 	}),
-	// });
-
-	// const fetchKoef = await response.json();
-	// console.log('fetchKoef ', fetchKoef);
-	// await sendMessage(chat.id, `Привет ${user.username}`);
-
-	console.log('process.env.WB_ACCEPTANCE_API_BOT ', process.env.WB_ACCEPTANCE_API_BOT);
-
-	let send = await sendMessage(305905070, 'Выполнение функции раз в 2 минут');
-	console.log('send Message ', send);
-
-	console.log('Received event! Next invocation at:');
+	if (data) {
+		data.forEach(async (req) => {
+			const response = await fetch('https://coef.wbcon.su/get_coef', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					wh_id: req.wh_id,
+					delivery_date: req.delivery_date,
+					delivery_type: req.delivery_type,
+				}),
+			});
+			const fetchKoef = await response.json();
+			let send = await sendMessage(305905070, `Коээфициент по складу ${req.wh_id} сейчас: ${fetchKoef.coef}`);
+			console.log('fetchKoef ', fetchKoef);
+			console.log('send ', send);
+		});
+	}
 };
