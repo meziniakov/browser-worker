a314b4d84564073c8a38734854d045207f3e1f2b
mezin <z2941@ya.ru>
2024-04-14 14:33:18 +0300

    fix

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -2,23 +2,31 @@ const sendMessage = require('../lib/telegram/wb_acceptance_rate/sendMessage');
 const client = require('../lib/supabase/wb_acceptance/client');
 
 export default async (req) => {
-	console.log('req ', req);
-	const { next_run } = await req.json();
+	console.log('req ', req.json());
+	// const { next_run } = await req.json();
 
-	const request = await client.from('requests').select('*');
+	const { data, error } = await client.from('requests').select('*');
 
-	console.log('request ', request[0]);
+	console.log('data ', data);
+	console.log('error ', error);
 
-	// fetch('https://coef.wbcon.su/get_coef', {
+	// const response = await fetch('https://coef.wbcon.su/get_coef', {
 	// 	method: 'POST',
 	// 	headers: {
-	// 		'Content-Type': 'application/json'
+	// 		'Content-Type': 'application/json',
 	// 	},
-	// 	body: JSON.stringify()
-	// })
+	// 	body: JSON.stringify({
+	// 		wh_id: request[0].wh_id,
+	// 		delivery_date: request[0].delivery_date,
+	// 		delivery_type: request[0].delivery_type,
+	// 	}),
+	// });
+
+	// const fetchKoef = await response.json();
+	// console.log('fetchKoef ', fetchKoef);
 
 	let send = await sendMessage(305905070, 'Выполнение функции раз в 5 минут');
 	console.log('send Message ', send);
 
-	console.log('Received event! Next invocation at:', next_run);
+	console.log('Received event! Next invocation at:');
 };
