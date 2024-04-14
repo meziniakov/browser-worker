711d2e13ce74175c36b79c827299d6e783dc540b
mezin <z2941@ya.ru>
2024-04-14 16:49:28 +0300

    test

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -3,11 +3,11 @@ const editMessage = require('../lib/telegram/wb_acceptance_rate/editMessage');
 const client = require('../lib/supabase/wb_acceptance/client');
 
 export default async (req) => {
-	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-	console.log('req ', await req.json());
-	let test = await sendMessage(305905070, `Пробное сообщение`);
+	// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+	// console.log('req ', await req.json());
+	// let test = await sendMessage(305905070, `Пробное сообщение`);
 
-	const { data, error } = await client.from('requests').select('*').eq('is_active', true);
+	// const { data, error } = await client.from('requests').select('*').eq('is_active', true);
 
 	const req = await fetch('https://jsonplaceholder.typicode.com/todos/1');
 	const res = await req.json();
@@ -28,8 +28,8 @@ export default async (req) => {
 	// 	}),
 	// });
 	// const fetchKoef = await response.json();
-	let send = await editMessage(305905070, test.result.message_id, null, `Коээфициент по складу ${data[0].wh_id} сейчас: ${res.title}`);
-	console.log('send ', send);
+	// let send = await editMessage(305905070, test.result.message_id, null, `Коээфициент по складу ${data[0].wh_id} сейчас: ${res.title}`);
+	// console.log('send ', send);
 	// console.log('fetchKoef ', fetchKoef);
 	// console.log('send ', send);
 
