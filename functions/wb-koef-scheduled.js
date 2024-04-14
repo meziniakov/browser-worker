a25e99d35aff8f03f945f45a9909d696cb10c6ba
mezin <z2941@ya.ru>
2024-04-14 15:02:10 +0300

    fix

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -2,27 +2,43 @@ const sendMessage = require('../lib/telegram/wb_acceptance_rate/sendMessage');
 const client = require('../lib/supabase/wb_acceptance/client');
 
 export default async (req) => {
-	console.log('req ', req.json());
+	console.log('req ', await req.json());
 
 	const { data, error } = await client.from('requests').select('*').eq('is_active', true);
 
-	if (data) {
-		data.forEach(async (req) => {
-			const response = await fetch('https://coef.wbcon.su/get_coef', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					wh_id: req.wh_id,
-					delivery_date: req.delivery_date,
-					delivery_type: req.delivery_type,
-				}),
-			});
-			const fetchKoef = await response.json();
-			let send = await sendMessage(305905070, `Коээфициент по складу ${req.wh_id} сейчас: ${fetchKoef.coef}`);
-			console.log('fetchKoef ', fetchKoef);
-			console.log('send ', send);
-		});
-	}
+	const response = await fetch('https://coef.wbcon.su/get_coef', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			wh_id: req.wh_id,
+			delivery_date: req.delivery_date,
+			delivery_type: req.delivery_type,
+		}),
+	});
+	const fetchKoef = await response.json();
+	let send = await sendMessage(305905070, `Коээфициент по складу ${req.wh_id} сейчас: ${fetchKoef.coef}`);
+	console.log('fetchKoef ', fetchKoef);
+	console.log('send ', send);
+
+	// if (data) {
+	// 	data.forEach(async (req) => {
+	// 		const response = await fetch('https://coef.wbcon.su/get_coef', {
+	// 			method: 'POST',
+	// 			headers: {
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 			body: JSON.stringify({
+	// 				wh_id: req.wh_id,
+	// 				delivery_date: req.delivery_date,
+	// 				delivery_type: req.delivery_type,
+	// 			}),
+	// 		});
+	// 		const fetchKoef = await response.json();
+	// 		let send = await sendMessage(305905070, `Коээфициент по складу ${req.wh_id} сейчас: ${fetchKoef.coef}`);
+	// 		console.log('fetchKoef ', fetchKoef);
+	// 		console.log('send ', send);
+	// 	});
+	// }
 };
