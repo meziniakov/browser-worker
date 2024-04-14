01154a50092ad6c6fea47ce07888077d68239420
mezin <z2941@ya.ru>
2024-04-14 14:14:21 +0300

    fix: sendMessage TG by cron

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -1,5 +1,10 @@
+import sendMessage from '../lib/telegram/wb_acceptance_rate/sendMessage';
+
 export default async (req) => {
+	console.log('req ', req);
 	const { next_run } = await req.json();
+
+	await sendMessage(305905070, 'Выполнение функции раз в 5 минут');
 
 	console.log('Received event! Next invocation at:', next_run);
 };
