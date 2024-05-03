3d5f29488ebba9205d8e6e0df77a01176bc3d03e
mezin <z2941@ya.ru>
2024-05-03 16:09:45 +0300

    fix bugs

--- functions/wb-koef-scheduled.js
+++ functions/wb-koef-scheduled.js
@@ -15,46 +15,42 @@ export default async (req) => {
 	if (data) {
 		data.forEach(async (req, n) => {
 			let delivery_date = new Date(req.delivery_date).toISOString();
-			let today = new Date().toISOString();
-
-			if (today <= delivery_date) {
-				try {
-					const response = await fetch('https://coef.wbcon.su/get_coef', {
-						method: 'POST',
-						agent: new https.Agent({
-							rejectUnauthorized: false,
-						}),
-						headers: {
-							'Content-Type': 'application/json',
-							'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
-						},
-						body: JSON.stringify({
-							wh_id: req.wh_id,
-							delivery_date: req.delivery_date,
-							delivery_type: req.delivery_type,
-						}),
-					});
-					const fetchKoef = await response.json();
-					console.log('fetchKoef ', fetchKoef);
-					if (fetchKoef?.coef <= req.coef) {
-						await client.from('requests').update({ is_active: false }).eq('id', req.id);
-						await sendMessage(
-							chat.id,
-							`Доступна приёмка:\n📦 › ${req.warehouses.title} › ${req.delivery_types.title} › ${req.coefficients.title} › ${new Date(
-								delivery_date
-							).toLocaleDateString('ru-RU')}\n`
-						);
-						await sendMessage(
-							305905070,
-							`Доступна приёмка:\n📦 › ${req.warehouses.title} › ${req.delivery_types.title} › ${req.coefficients.title} › ${new Date(
-								delivery_date
-							).toLocaleDateString('ru-RU')}\n`
-						);
-					}
-					setTimeout(() => {}, 3000);
-				} catch (e) {
-					console.log('Error fetch: ', e);
+			try {
+				const response = await fetch('https://coef.wbcon.su/get_coef', {
+					method: 'POST',
+					agent: new https.Agent({
+						rejectUnauthorized: false,
+					}),
+					headers: {
+						'Content-Type': 'application/json',
+						'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+					},
+					body: JSON.stringify({
+						wh_id: req.wh_id,
+						delivery_date: req.delivery_date,
+						delivery_type: req.delivery_type,
+					}),
+				});
+				const fetchKoef = await response.json();
+				console.log('fetchKoef ', fetchKoef);
+				if (fetchKoef?.coef <= req.coef) {
+					await client.from('requests').update({ is_active: false }).eq('id', req.id);
+					await sendMessage(
+						chat.id,
+						`Доступна приёмка:\n📦 › ${req.warehouses.title} › ${req.delivery_types.title} › ${req.coefficients.title} › ${new Date(
+							delivery_date
+						).toLocaleDateString('ru-RU')}\n`
+					);
+					await sendMessage(
+						305905070,
+						`Доступна приёмка:\n📦 › ${req.warehouses.title} › ${req.delivery_types.title} › ${req.coefficients.title} › ${new Date(
+							delivery_date
+						).toLocaleDateString('ru-RU')}\n`
+					);
 				}
+				setTimeout(() => {}, 3000);
+			} catch (e) {
+				console.log('Error fetch: ', e);
 			}
 		});
 	}
